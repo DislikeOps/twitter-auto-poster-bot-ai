571b503907bfcd30e32c619917a6640b868cbd1b
DislikeOps <dislikeops@proton.me>
2024-11-07 15:42:30 -0600

    Create secrets.js

---
+++ secrets.js
@@ -0,0 +1,18 @@
+//? It is advisable to use environment variables instead of directly putting secrets in repository file but I have skipped this part as it would become complicated for many.
+//? Alternatively you can download the repository instead or forking and upload it from your account and keep it private, in that way, your secrets will not be exposed to the public.
+
+const APP_KEY = "8EjdgUXcdNpORxz3NUt5VzP06";
+const APP_SECRET = "gBtGGR5qZvxe8gsKTf1Gt0L0mpY1TYPTisA9iHxH0pdRI3O8uX";
+const ACCESS_TOKEN = "1854261263058055179-ogRcH98YRO7wzMKaW4vh5oC4QrTcab";
+const ACCESS_SECRET = "EpCa2z1K8FrSjFncPt3oKQl6mEW1qX8IxrerB9AhZOXdh";
+const GEMINI_API_KEY = "AIzaSyAx8dnlJVYa4i_lrwzWN8ffwFBQcg0NY_M";
+
+const SECRETS = {
+  APP_KEY,
+  APP_SECRET,
+  ACCESS_TOKEN,
+  ACCESS_SECRET,
+  GEMINI_API_KEY,
+};
+
+module.exports = SECRETS;
